8222a310be4ccdd90e579b2330aa2e8fb53fca76
Nhi Eudy <nhieudy@Mac.attlocal.net>
2025-09-19 00:32:41 -0500

    Added underscores to replace word

--- index.js
+++ index.js
@@ -13,9 +13,15 @@ var words = [
 ];
 //Generate a random word, based on the words array
 var randomWord = words[Math.floor(Math.random() * words.length)];
-console.log(randomWord);
 //Place the word into the word-to-guess element, with underscores
 //Should also display ten remaining guesses in #remaining-guesses
 var wordToGuess = document.querySelector("#word-to-guess");
-wordToGuess.textContent = randomWord;
+wordToGuess.textContent = randomWord; //wordToGuess content set to randomWord
+
+var unguessedWord = "";
+for(var i =0; i<randomWord.length; i++){
+ unguessedWord += "_";
+}
+wordToGuess.textContent = unguessedWord;
+
 
