a00e441d74d70fa5c93199b25207770993b71c20
Nhi Eudy <nhieudy@Mac.attlocal.net>
2025-09-19 03:11:11 -0500

    Guessed letter appears, counter works

--- index.js
+++ index.js
@@ -13,15 +13,40 @@ var words = [
 ];
 //Generate a random word, based on the words array
 var randomWord = words[Math.floor(Math.random() * words.length)];
+console.log(randomWord);
 //Place the word into the word-to-guess element, with underscores
 //Should also display ten remaining guesses in #remaining-guesses
+//Guesses
+var guessCounter = 10;
+var guesses = document.querySelector("#remaining-guesses");
+guesses.textContent = guessCounter;
+//Underscore word
 var wordToGuess = document.querySelector("#word-to-guess");
-wordToGuess.textContent = randomWord; //wordToGuess content set to randomWord
-
 var unguessedWord = "";
-for(var i =0; i<randomWord.length; i++){
- unguessedWord += "_";
+for(let i =0; i<randomWord.length; i++){
+ unguessedWord += "_"; //adds _ to the end of the string for the length of the word
 }
 wordToGuess.textContent = unguessedWord;
+
+//Keyboard Input
+var arrayUnguessed = unguessedWord.split("");
+document.addEventListener('keyup', function(e){
+//Guess counter
+var updatedWord = "";
+guesses.textContent = guessCounter;
+var guess = e.key.toLowerCase();
+for (let i = 0; i<randomWord.length; i++){
+  if(guess === randomWord[i]){
+    arrayUnguessed[i] = randomWord[i];
+    unguessedWord = arrayUnguessed.join("");//No commas
+  }
+  else{
+guessCounter = guessCounter -1;
+  }
+}
+wordToGuess.textContent = unguessedWord;
+}
+)
+
 
 
